f8d824478211970a07338751b8b8a77e9d7229c4
Lucas Garcia <33587653+LucasGarcia07@users.noreply.github.com>
2020-06-26 16:35:34 -0300

    Update DistanceSimilarity.jl

--- src/DistanceSimilarity.jl
+++ src/DistanceSimilarity.jl
@@ -36,18 +36,6 @@ function cosine(x, y)
     if length(x) != length(y)
         throw(ArgumentError("The vectors must be the same size"))
     end
-
-    valor1, valor2, valor3 = 0, 0, 0
     scalar, xSquared, ySquared  = x .* y, x .^ 2, y .^ 2
-
-    for i = 1:length(x)
-        valor1 += scalar[i]
-        valor2 += xSquared[i]
-        valor3 += ySquared[i]
-    end
-    valor1/(sqrt(valor2)*sqrt(valor3))
-end
-x = [3, 0]
-y = [0, 3]
-
+    sum(scalar)/(sqrt(sum(xSquared))*sqrt(sum(ySquared)))
 end # module
